ff7ff95977d3f954261571ee6734ada46ae324c3
Tomas Vicar <40293035+tomasvicar@users.noreply.github.com>
2025-04-25 10:26:47 +0000

    nto working

--- start_xpra.sh
+++ start_xpra.sh
@@ -1,8 +1,12 @@
-# chmod +x start_xpra.sh 
-# ./start_xpra.sh
+# . start_xpra.sh or source start_xpra.sh
+
+export XDG_RUNTIME_DIR=/tmp/$UID-runtime
+mkdir -p $XDG_RUNTIME_DIR
+chmod 700 $XDG_RUNTIME_DIR
 xpra start :100      --bind-tcp=0.0.0.0:14500      --html=on
 export DISPLAY=:100
 export XDG_RUNTIME_DIR=/tmp/$UID-runtime
 
 # and run python script
+
 # xpra stop :100
